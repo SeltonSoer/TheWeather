ae2cfc8c37bf8694f0852670673548cae56982a9
Даниленко Алексей Викторович <a_danilenko@controlsystems.ru>
2024-11-14 01:17:05 +0300

    v1.1

--- backend/myRequests/weatherRequests.py
+++ backend/myRequests/weatherRequests.py
@@ -1,12 +1,10 @@
 import requests
-# import json
-
-# 'http://api.weatherapi.com/v1/current.json?key=<YOUR_API_KEY>&q=London'
+import os
+import json
 
 class Weather:
 
-    # API-key add to PATH in system
-    api_key = 'testKey'
+    api_key = os.getenv('API_WEATHER_KEY')
 
     def get_weather(self, city):
         request = requests.get(f'http://api.weatherapi.com/v1/current.json?key={self.api_key}&q={city}')
@@ -14,7 +12,9 @@ class Weather:
         if request.status_code == 200:
             data = request.json()
 
-            # formatted_weather_info = json.dumps(data, indent=4, ensure_ascii=False)
-            # print(formatted_weather_info)
+            formatted_weather_info = json.dumps(data, indent=4, ensure_ascii=False)
+            print(formatted_weather_info)
 
             return data['current']
+        else:
+            print(request)
